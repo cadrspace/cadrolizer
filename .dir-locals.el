476fcd29801847959482a5a5bd1ad8c712490760
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2015-05-31 06:30:45 +0300

    .dir-locals.el: Set style for c++-mode

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,8 +1,8 @@
 ;;; .dir-locals.el -- Per-directory local variables for GNU Emacs 23 and later.
 
-((nil         . ((fill-column  . 78)
-                 (tab-width    .  8)))
- (c-mode      . ((c-file-style . "linux"))))
+((nil           . ((fill-column  . 78)
+                   (tab-width    .  8)))
+ (c++-mode      . ((c-file-style . "linux"))))
 
 ;;; .dir-locals.el ends here
 
